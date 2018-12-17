898bb31d334f15a15d7653fb9f54aa313664c523
sgaarchopada <sagar.chopada@bcssarl.com>
2018-12-17 15:55:33 +0530

    add nodejs

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/",(req,res)=>{
+	res.sendFile(__dirname+"/index.html");
+});
+
+app.listen(3000,function(){
+	console.log("app start listening at 3000 port");
+});
